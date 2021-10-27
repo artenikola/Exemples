ae95eb3a5eaf1f5fbeb3520a2200f2d63a8f5ea0
artenikola <artenikola@yandex.ru>
2021-10-27 23:03:10 +0300

    Example_006_Base

---
+++ Examples/Example_003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int NumberA = new Random().Next(1,10065464); // 1 2 3 4 5 6 7 8 9 10
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,17000);
+Console.WriteLine(NumberB);
+int result = NumberA + NumberB;
+Console.WriteLine(result);
