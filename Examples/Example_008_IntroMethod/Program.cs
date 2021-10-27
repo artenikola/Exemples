95ef7df28572154e4f6de7e45c46347c96d275f9
artenikola <artenikola@yandex.ru>
2021-10-28 01:26:25 +0300

    Example_010_MethodArra

---
+++ Examples/Example_008_IntroMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 11;
+int b1 = 45;
+int c1 = 12;
+int a2 = 3335;
+int b2 = 42;
+int c2 = 222;
+int a3 = 332;
+int b3 = 23335;
+int c3 = 24;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1,max2,max3);
+
+Console.WriteLine(max);
---
+++ Examples/Example_010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 0, 6, 0, 43, 21, 67, 76};
+
+int n = array.Length;
+int find = 0;
+
+int index = 0;
+while (index < n)
+{ 
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1
+    index ++;
+
+}
